5222514513b409f5fdd45f35d9cae90acf32a2f8
anilkharatmol <122686374+anilkharatmol@users.noreply.github.com>
2023-03-26 23:03:29 +0530

    Making apps error proof

--- API calls in React/src/App.js
+++ API calls in React/src/App.js
@@ -8,23 +8,47 @@ function App() {
 
 const[movies,setMovies]=useState([]);
 
+const[isLoading,setIsLoading]=useState(false);
+
+const[error,setError]=useState(null);
+
+const[retry,setRetry]=useState(false);
+
  async function fetchMoviesHandler(){
   
-
-  const response=await  fetch('https://swapi.dev/api/films');
+   setError(null);
+  try {
+  const response=await  fetch('https://swapi.dev/api/film');
+  if (!response.ok){
+    throw new Error('Something went wrong...Retrying');
+  }
   const data= await response.json();
 
-  
+
     const transformedMovies=data.results.map((movieData)=>{
+
+    
       return{
       id:movieData.episode_id,
       title:movieData.title,
       openingText:movieData.opening_crawl,
       releaseDate:movieData.release_date
        }
-  });
-    setMovies(transformedMovies)
+      });
+      setMovies(transformedMovies)
+    }
+    
+    catch (error) {
+      setError(error.message);
+      setInterval(() => {
+       setRetry(true);
+      }, 5000);
+    }
+    
+
+    setIsLoading(false);
 }
+   
     
     return (
       <React.Fragment>
@@ -32,7 +56,11 @@ const[movies,setMovies]=useState([]);
         <button onClick={fetchMoviesHandler}>Fetch Movies</button>
       </section>
       <section>
-     <MoviesList movies={movies} />
+        {isLoading &&<h1>LOADING....</h1>}
+        {retry}
+      {!isLoading && <MoviesList movies={movies} />}
+      {!isLoading && error && <div><h1>{error}</h1>
+      <button onClick={()=>{setIsLoading(true)}}>Cancel</button></div>}
       </section>
     </React.Fragment>
   );
