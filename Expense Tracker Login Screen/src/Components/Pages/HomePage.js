12b5876fb6d9f85cd4566a9c46036686154b68f1
anilkharatmol <122686374+anilkharatmol@users.noreply.github.com>
2023-04-12 16:52:16 +0530

    Expense Tracker Login Screen

---
+++ Expense Tracker Login Screen/src/Components/Pages/HomePage.js
@@ -0,0 +1,10 @@
+
+
+export default function HomePage(){
+    return(
+        <div>
+            <h2 style={{fontFamily:'cursive'}}> Welcome to Expense Tracker!!!</h2>
+            <hr></hr>
+        </div>
+    )
+}
